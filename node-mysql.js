c8eb7b1712dccb266bba597c3553ad93532e2e02
KiranPurbey1993 <purbey.kiran.com>
2022-02-24 22:56:20 +0530

    done

--- node-mysql.js
+++ node-mysql.js
@@ -1,12 +1,26 @@
 var mysql = require('mysql2');
-var con = mysql.createConnection({
+
+var connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "password",
     database: "mydb"
   });
 
-  con.connect((err) => {
+  
+connection.connect((err) => {
   if (err) throw err;
   console.log('Connected to MySQL Server!');
+});
+
+app.get("/",(req,res) => {
+    connection.query('SELECT * from users LIMIT 1', (err, rows) => {
+        if(err) throw err;
+        console.log('The data from users table are: \n', rows);
+        connection.end();
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is running at port 3000');
 });
